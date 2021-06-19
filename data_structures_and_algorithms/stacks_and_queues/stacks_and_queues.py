b4b39765a4df07484911d8ee4380957e2e114650
MohmmadNada <mohammad.nada_97@yahoo.com>
2021-06-19 03:17:47 +0300

    finish code, still we need the sumbtion inst

--- data_structures_and_algorithms/stacks_and_queues/stacks_and_queues.py
+++ data_structures_and_algorithms/stacks_and_queues/stacks_and_queues.py
@@ -1,4 +1,6 @@
 # should add class node and LL from 
+import re
+# from tests.test_stacks import test_empty_stock
 from data_structures_and_algorithms.Data_Structures.linked_list.linked_list import Node,LinkedList
 # class Node :
 #     '''
@@ -68,14 +70,77 @@ class Stack :
         else:
             return False
 
-
+class Queue :
+    def __init__(self):
+        '''
+        This object should be aware of a default empty value assigned to front when the queue is created.
+        '''
+        self.front=None
+        self.rear=None
+    def enqueue(self,value):
+        '''
+         takes any value as an argument and adds a new node with that value to the back of the queue with an O(1) Time performance.
+        '''
+        newNode=Node(value)
+        if self.rear==None:
+            self.rear=newNode
+            self.front=newNode
+        else:
+            self.rear.next=newNode
+            self.rear=newNode
+    def dequeue (self):
+        '''
+        Define a method called dequeue that does not take any argument, removes the node from the front of the queue, and returns the node’s value. Should raise exception when called on empty queue
+        '''
+        try:
+            if self.rear==None:
+                raise Exception('THIS Queue is Empty')
+            else:
+                temp=self.front
+                self.front=self.front.next
+                temp.next=None
+            return temp.value
+        except ValueError:
+            return 'Oops  , empty Queue'
+    def peek(self):
+        '''
+        does not take an argument and returns the value of the node located in the front of the queue, without removing it from the queue. Should raise exception when called on empty queue
+        '''
+        try:
+            if self.front==None:
+                raise Exception('Oop`s This Queue is Empty ')
+            else:
+                temp=self.front
+                return temp.value
+        except AttributeError:
+            return 'the queue is empty '
+    def isEmpty(self):
+        '''
+        Define a method called isEmpty that takes no argument, and returns a boolean indicating whether or not the queue is empty.
+        '''
+        if self.front==None:
+            return True
+        else:
+            return False
 if __name__=='__main__':
-    stock=Stack()
-    print('---------')
+    # stock=Stack()
+    # print('---------')
     
-    stock.push(5)
-    stock.push('a')
-    stock.push('50')
-    stock.push('LIFO')
-    print(stock.peek())
-    # exception desont work 
+    # stock.push(5)
+    # stock.push('a')
+    # stock.push('50')
+    # stock.push('LIFO')
+    # print(stock.peek())
+    # # exception desont work 
+    queue=Queue()
+    print('is it true ?',queue.isEmpty())
+    # print('------- raise--- ',queue.peek())# front aa   
+    queue.enqueue('a')# a=front
+    queue.enqueue('aa')# front a  aa
+    queue.enqueue('0')# front a  aa 0 
+    print(queue.front.value)
+    print(queue.front.next.value)
+    print(queue.front.next.next.value)
+    print('----',queue.dequeue()) # front aa 0 
+    print('------- peek --- ',queue.peek())# front aa
+    print('is it true ?',queue.isEmpty())
