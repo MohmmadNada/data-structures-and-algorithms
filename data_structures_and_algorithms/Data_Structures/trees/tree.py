b0149fcb7c65b6c43cb36f0b57cba0c7254447ed
MohmmadNada <mohammad.nada_97@yahoo.com>
2021-06-26 00:15:14 +0300

    finish pre and post order without test

--- data_structures_and_algorithms/Data_Structures/trees/tree.py
+++ data_structures_and_algorithms/Data_Structures/trees/tree.py
@@ -1,3 +1,7 @@
+import pysnooper
+import re
+
+
 '''
 * Create a Binary Tree class
     Define a method for each of the depth first traversals:
@@ -5,8 +9,6 @@
         in order
         post order which returns an array of the values, ordered appropriately.
 * Any exceptions or errors that come from your code should be semantic, capture-able errors. For example, rather than a default error thrown by your language, your code should raise/throw a custom, semantic error that describes what went wrong in calling the methods you wrote for this lab.
-'''
-'''
 Structure and Testing
 
 * Done
@@ -20,7 +22,6 @@ Structure and Testing
 
 # depth first , pre order , from last depth , from left to right , use recursion
 
-import re
 
 
 class Node:
@@ -58,22 +59,62 @@ class BinaryTree:
                 return outputStr
             _traverse()
         return outputStr
+    def inOrder(self):
+        node = self.root
+        outputStr=''
+        if self.root==None:
+            return ''
+        else: 
+            # @pysnooper.snoop()  
+            def _traverse(node):
+                nonlocal outputStr
+                if node.left:
+                    _traverse(node.left)
+                outputStr+=str(node.value)
+                if node.right:
+                    _traverse(node.right)
+            _traverse(node)
+        return outputStr
+    def postOrder(self):
+        node=None or self.root 
+        outputStr=''
+        if node!=None:
+            # left >> right >> root
+            def _traverse(node):
+                nonlocal outputStr
+                if node.left:
+                    _traverse(node.left)
+                if node.right:
+                    _traverse(node.right)
+                outputStr+=str(node.value)
+                return outputStr
+            _traverse(node)
+        return outputStr
+
+
+
+
+
+
 
 if __name__=='__main__':
     Btree=BinaryTree()
     # print(Btree.root)
-    Btree.root=Node('rootItem')
+    Btree.root=Node('rootItem ')
     # print('the root value is =>               ',Btree.root.value) # root 'rootItem'             
-    Btree.root.right=Node('rightRoot')
-    Btree.root.left=Node('leftRoot')
+    Btree.root.right=Node('rightRoot ')
+    Btree.root.left=Node('leftRoot ')
     rightRoot=Btree.root.right
     leftRoot=Btree.root.left
     # print(leftRoot.value, '                             ********                ',rightRoot.value)
     # print()
-    leftRoot.left=Node('left_left')
-    leftRoot.right=Node('left_right')
-    rightRoot.left=Node('right_left')
-    rightRoot.right=Node('right_right')
-    rightRoot.right.right=Node('right_right_right')
+    leftRoot.left=Node('left_left ')
+    leftRoot.right=Node('left_right ')
+    rightRoot.left=Node('right_left ')
+    rightRoot.right=Node('right_right ')
+    rightRoot.right.right=Node('right_right_right ')
     # print(leftRoot.left.value , '     ',leftRoot.right.value, '         ***************         ',rightRoot.left.value,'     ',rightRoot.right.value)
-    print(Btree.preOrder())
+    # print(Btree.preOrder())
+    # Btree.inOrder()
+    # print(Btree.inOrder())
+    print(Btree.postOrder())
